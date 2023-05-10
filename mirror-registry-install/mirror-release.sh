f4e73e1e88b408b56ccd61e2ca1d61a43c183b07
jcl <jordi@redhat.com>
2023-05-10 02:28:51 +0200

    fixing mirror-release script

--- mirror-registry-install/mirror-release.sh
+++ mirror-registry-install/mirror-release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Setting 'set -e' to stop the script if any command fails
+# Stop the script if any command fails
 set -e
 
 # Function to handle errors
@@ -9,13 +9,22 @@ error_exit() {
   exit "${2:-1}"
 }
 
+# Ensure necessary environment variables are set
+if [[ -z "${QUAY_HOSTNAME}" ]] || [[ -z "${QUAY_USER}" ]] || [[ -z "${QUAY_PASSWORD}" ]]; then
+  error_exit "One or more required environment variables (QUAY_HOSTNAME, QUAY_USER, QUAY_PASSWORD) are not set. Please check your settings."
+fi
+
+# Export PATH
+export PATH=$PATH:.
+
 # Crafting an ImageSet with designated minimum and maximum Openshift versions
+echo "Creating ImageSet configuration..."
 cat << EOF > imageset-config.yaml
 kind: ImageSetConfiguration
 apiVersion: mirror.openshift.io/v1alpha2
 storageConfig:
   registry:
-    imageURL: $QUAY_HOSTNAME/openshift4
+    imageURL: https://$QUAY_HOSTNAME/openshift4
     skipTLS: false
 mirror:
   platform:
@@ -26,36 +35,22 @@ mirror:
       type: ocp
 EOF
 
-# Checking for any error while creating ImageSet
-if [ $? -ne 0 ]; then
-  error_exit "Failed to create ImageSet. Please check your configurations."
-fi
-
-echo "Successfully crafted the ImageSet. Proceeding to the mirroring phase, which might take some time."
-
-# Export PATH
-export PATH=$PATH:.
-
-# Commencing the mirroring of the OpenShift release
-chmod 755 oc-mirror
-oc-mirror --config=./imageset-config.yaml docker://$QUAY_HOSTNAME/openshift4
-
-# Checking for any error during the mirroring process
-if [ $? -ne 0 ]; then
-  error_exit "Failed to mirror the OpenShift release. Please check your network connection or configurations."
-fi
-
-echo "Mirroring process completed. Now, let's generate the necessary authentication file."
-
 # Generating the auth.json authentication file
+echo "Generating authentication file..."
 AUTH=$(echo -n "${QUAY_USER}:${QUAY_PASSWORD}" | base64 -w0) || error_exit "Failed to encode credentials."
 cat << EOF > auth.json
 {"auths":{"$QUAY_HOSTNAME":{"auth":"$AUTH"}}}
 EOF
 
-# Checking for any error while creating the authentication file
-if [ $? -ne 0 ]; then
-  error_exit "Failed to create the authentication file. Please check your credentials."
-fi
+# Copy the auth.json file to the Docker config location
+mkdir -p ~/.docker
+cp auth.json ~/.docker/config.json || error_exit "Failed to copy the authentication file to the Docker config location."
 
 echo "Authentication file successfully generated and ready for use."
+
+# Commencing the mirroring of the OpenShift release
+echo "Starting the mirroring process. This may take a while..."
+chmod 755 oc-mirror
+./oc-mirror --config=./imageset-config.yaml docker://$QUAY_HOSTNAME/openshift4 || error_exit "Failed to mirror the OpenShift release. Please check your network connection or configurations."
+
+echo "Mirroring process completed successfully."
