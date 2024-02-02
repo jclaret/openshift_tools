75be7a3eb698c067cc981b3f37418f23ab2708e5
jcl <jordi@redhat.com>
2024-02-02 14:59:39 +0100

    adding odf stuff

--- odf_lso/odf/check.bash
+++ odf_lso/odf/check.bash
@@ -1,6 +1,4 @@
-# Approve Install plan
-oc patch installplan install-dq68d --namespace openshift-storage --type merge --patch '{"spec":{"approved":true}}'
-# Noobaa issues # Noobaa issues https://www.noobaa.io/noobaa-crd.html
+# Noobaa issues https://www.noobaa.io/noobaa-crd.html
 oc patch OCSInitialization ocsinit -n openshift-storage --type json --patch  '[{ "op": "replace", "path": "/spec/enableCephTools", "value": true }]'
 TOOLS_POD=$(oc get pods -n openshift-storage -l app=rook-ceph-tools -o name)
 oc rsh -n openshift-storage $TOOLS_POD
--- odf_lso/odf/install.bash
+++ odf_lso/odf/install.bash
@@ -1,4 +1,3 @@
-# kcli update vm rna1-vmaster-{0..2} -P numcpus=64 -P memory=135168
 oc create -f 01-odf-operator.yaml
 oc create -f 02-StorageCluster.yaml
 oc create -f 03-rbd-pvc.yaml
