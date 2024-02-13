ab44a67bb53a9e0ad1f7c3466271616b05a3df8a
jcl <jordi@redhat.com>
2024-02-13 15:18:22 +0100

    fixes

--- ocp_deployments/SNO/install.bash
+++ ocp_deployments/SNO/install.bash
@@ -1,1 +1,2 @@
+# kcli create network -c 192.168.126.0/24 -P dhcp=false -P dns=false -d 2620:52:0:1306::0/64 --domain 5g-deployment.lab --nodhcp 5gdeploylabdual
 kcli create cluster openshift --pf ./sno.yaml
